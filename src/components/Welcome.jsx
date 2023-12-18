3f8ca734db1601f50f53daf64802580ac3b724fd
ajaalwandawy <147595458+ajaalwandawy@users.noreply.github.com>
2023-12-18 11:14:57 +0100

    !

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 export default function Welcome() {
   return (
     <>
-      <h1>Hej o välkommen!</h1>
+      <h1>Hej o välkommen! !</h1>
       <p>Detta är startsidan med lite smått o gott</p>
     </>
   );
