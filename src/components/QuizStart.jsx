fb10e75671b5cd48e07e775c23eff3c354e73ab4
Yooniis <151764628+Yooniis@users.noreply.github.com>
2023-12-19 09:54:36 +0100

    Test att lägga in innehåll på svårighetsgraderna

--- src/components/QuizStart.jsx
+++ src/components/QuizStart.jsx
@@ -10,13 +10,21 @@ export default function QuizStart() {
             <Col lg={5} xl={4} >
             <Card style={{ height: CARD_HEIGHT}}>
                 <h2>Lätt som en plätt</h2>
-                <p>Reglerna lyder!</p>
+                <p>Perfekt för dig som precis börjat lära dig om klimatförändringar och miljöfrågor</p>
+
+                <h2>Spelregler</h2>
+                <p>Antal frågor: 10 frågor</p>
+                <p>Du har obegränsat med tid</p>
             </Card>
             </Col>
             <Col lg={5} xl={4} >
             <Card style={{ height: CARD_HEIGHT}}>
                 <h2>Svårt o hårt</h2>
-                <p>Reglerna lyder!</p>
+                <p>För dig som känner dig säker på dina kunskaper och vill ha en utmaning</p>
+
+                <h2>Spelregler</h2>
+                <p>Antal frågor: 15 frågor</p>
+                <p>Du har 15 minuter på dig</p>
             </Card>
             </Col>
         </Row>
