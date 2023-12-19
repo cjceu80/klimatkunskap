153eaadac78d6f5b2db0845611b6e6681216d797
Yooniis <151764628+Yooniis@users.noreply.github.com>
2023-12-19 10:05:37 +0100

    padding

--- src/components/QuizStart.jsx
+++ src/components/QuizStart.jsx
@@ -8,7 +8,7 @@ export default function QuizStart() {
     <Container className="d-flex flex-column h-100" >
         <Row className="justify-content-md-center align-items-center h-100">
             <Col lg={5} xl={4} >
-            <Card style={{ height: CARD_HEIGHT}}>
+            <Card style={{ height: CARD_HEIGHT}}className="p-5">
                 <h2>Lätt som en plätt</h2>
                 <p>Perfekt för dig som precis börjat lära dig om klimatförändringar och miljöfrågor</p>
 
@@ -18,7 +18,7 @@ export default function QuizStart() {
             </Card>
             </Col>
             <Col lg={5} xl={4} >
-            <Card style={{ height: CARD_HEIGHT}}>
+            <Card style={{ height: CARD_HEIGHT}} className="p-5">
                 <h2>Svårt o hårt</h2>
                 <p>För dig som känner dig säker på dina kunskaper och vill ha en utmaning</p>
 
