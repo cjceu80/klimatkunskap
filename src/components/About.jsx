8932d0eaedf79fd2fbc962de880a50d0b534eea7
ndaivan <ivan.ndayizeye.github2023@gmail.com>
2023-12-20 10:45:06 +0100

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import logo from './Pictures/logga7.png';
 
 export default function About() {
   return (
-    <>
-      <h1>Om oss</h1>
-      <p>Detta är en sida om oss</p>
+    
+    <>  
+     
+     <img src={logo} alt=""></img>
+    <br/>
+
+      <h1>Teamet bakom klimatkunskap applikation</h1>
+      <br/>
+
+      <p>Vår berättelse började i en svensk högstadieskola, 
+        där 20 unika elever inspirerade oss att skapa KlimatKunskap - 
+        en applikation med hjärta och engagemang för att göra lärandet om klimatförändringarna roligt, 
+        interaktivt och relevant för unga sinnen.</p>
+      <p>Vår resa fortsätter med användningen av de senaste 
+        API:erna för att säkerställa att vår information är så aktuell som möjligt.</p>
+      <p>Tillsammans strävar vi efter att göra KlimatKunskap applikation till en resurs som inte bara lär, 
+        utan också inspirerar. 
+        Tack för att ni är med oss på den här resan!</p>
+      
     </>
   );
 }
