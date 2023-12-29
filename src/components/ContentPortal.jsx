d2ed39cfd2b09c0c901b685a0f131f24b87c6b3d
ajaalwandawy <147595458+ajaalwandawy@users.noreply.github.com>
2023-12-29 02:02:19 +0100

    Aja

--- src/components/ContentPortal.jsx
+++ src/components/ContentPortal.jsx
@@ -1,10 +1,128 @@
 import React from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import fossiltImg from "../images/fossil-fuel 1.svg";
+import meltingImg from "../images/melting 1.svg";
+import waterlevelImg from "../images/water-level 1.svg";
+import temperatureImg from "../images/temperature 1.svg";
+import "../style.css";
 
 export default function ContentPortal() {
+  const cardStyle = {
+    background: "rgba(255, 255, 255, 0.7)", // 0.7 represents 70% transparency
+    borderRadius: "25px",
+    marginBottom: "100px",
+  };
+  const cardImageStyle = {
+    height: "80px", // Set a specific height for the images
+    objectFit: "contain", // Use "cover" to maintain aspect ratio and cover the entire container
+  };
+
   return (
     <>
       <div className="bgContentPortal">
-        <a href="#" className="infoCardFossil">
+        <Container fluid className="h-100 pageContainer mb-5">
+          <Row>
+            <Col
+              xs={6}
+              md={4}
+              className="text-center mb-3 pt-5"
+              style={{ marginRight: "0px" }}
+            >
+              <a href="#" style={{ textDecoration: "none" }}>
+                <Card className="h-100 w-100 custom-card" style={cardStyle}>
+                  <Card.Body className="h-100">
+                    <div className="placeholder" />
+                    <Card.Title className="mb-3">
+                      <h1>Fossilt bränsle</h1>
+                    </Card.Title>
+                    <Card.Img
+                      variant="top"
+                      src={fossiltImg} //URL of the image
+                      style={cardImageStyle}
+                      className="mb-3"
+                    />
+                  </Card.Body>
+                </Card>
+              </a>
+            </Col>
+
+            <Col
+              xs={6}
+              md={4}
+              className="text-center mb-3 pt-5"
+              style={{ marginRight: "0px" }}
+            >
+              <a href="#" style={{ textDecoration: "none" }}>
+                <Card className="h-100 w-100 custom-card" style={cardStyle}>
+                  <Card.Body className="h-100">
+                    <div className="placeholder" />
+                    <Card.Title className="mb-3">
+                      <h1>Temperaturförändring</h1>
+                    </Card.Title>
+                    <Card.Img
+                      variant="top"
+                      src={temperatureImg} //URL of the image
+                      style={cardImageStyle}
+                      className="mb-3"
+                    />
+                  </Card.Body>
+                </Card>
+              </a>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col
+              xs={6}
+              md={4}
+              className="text-center mb-3 pt-3"
+              style={{ marginRight: "0px" }}
+            >
+              <a href="#" style={{ textDecoration: "none" }}>
+                <Card className="h-100 w-100 custom-card" style={cardStyle}>
+                  <Card.Body className="h-100">
+                    <div className="placeholder" />
+                    <Card.Title className="mb-3">
+                      <h1>Havsnivå</h1>
+                    </Card.Title>
+                    <Card.Img
+                      variant="top"
+                      src={waterlevelImg} //URL of the image
+                      style={cardImageStyle}
+                      className="mb-3"
+                    />
+                  </Card.Body>
+                </Card>
+              </a>
+            </Col>
+
+            <Col
+              xs={6}
+              md={4}
+              className="text-center mb-3 pt-3"
+              style={{ marginRight: "0px" }}
+            >
+              <a href="#" style={{ textDecoration: "none" }}>
+                <Card className="h-100 w-100 custom-card" style={cardStyle}>
+                  <Card.Body className="h-100">
+                    <div className="placeholder" />
+                    <Card.Title className="mb-3">
+                      <h1>Glaciärer</h1>
+                    </Card.Title>
+                    <Card.Img
+                      variant="top"
+                      src={meltingImg} //URL of the image
+                      style={cardImageStyle}
+                      className="mb-3"
+                    />
+                  </Card.Body>
+                </Card>
+              </a>
+            </Col>
+          </Row>
+        </Container>
+        {/* <div className="bgContentPortal">
+        {/* <a href="#" className="infoCardFossil">
           Fossilt bränsle
         </a>
 
@@ -18,7 +136,7 @@ export default function ContentPortal() {
 
         <a href="#" className="infoCardGlaciar">
           Glaciärer
-        </a>
+        </a> */}
       </div>
     </>
   );
