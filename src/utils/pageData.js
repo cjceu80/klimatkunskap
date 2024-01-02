3c83a9c8f410baa2f578f176b76c2764c97b86e7
Yooniis <151764628+Yooniis@users.noreply.github.com>
2024-01-02 15:30:41 +0100

    Jonas

--- src/utils/pageData.js
+++ src/utils/pageData.js
@@ -14,34 +14,42 @@ const pageData = [
         name: "Havsnivå",
         cards: [
             {
-                image: "/src/images/facepalm-facepalm-through-head_small.png",
+                image: "/src/images/havsniva/VattenVagor.png",
                 imageWidth: 3,
                 caption: "Varför blir havsnivån högre?",
                 paragraphs: ["Havet blir högre på grund av varmare klimat. Isar smälter och vattnet i haven blir större.","I Stockholm höjs landet fortfarande mer än havet, så det känns som om havet sjunker jämfört med landet."]
             },
             {
-                image: "/src/images/facepalm-facepalm-through-head_small.png",
+                image: "/src/images/havsniva/VattenVagor.png",
                 imageWidth: 2,
                 caption: "Påverkar global uppvärmning?",
                 paragraphs: ["Global uppvärmning påverkar havet genom att höja vattennivån när det blir varmare. Smältande isar från Grönland och Antarktis bidrar också till detta.","Geografiska skillnader och landhöjd skapar variationer i havsnivån över olika regioner i världen."]
             },
             {
+                image: "/src/images/havsniva/VattenVagor.png",
+                imageWidth: 3,
                 caption: "Vem har koll?",
                 paragraphs: ["SMHI har information om hur högre hav kan påverka Sverige. Det handlar om framtida vattenstånd och extrema vattennivåer. De beräknar för alla kuststäder i Sverige och ger även information om höjningar efter år 2150."]
             },
             {
+                image: "/src/images/havsniva/VattenVagor.png",
+                imageWidth: 3,
                 caption: "Hur mycket stiger havet?",
                 paragraphs: ["Havet stiger redan nu. Mellan 2006 och 2018 ökade havsnivån i genomsnitt med 3,7 mm per år.", "I Stockholm höjs landet med över 5 mm varje år, så det känns som om havsnivån sjunker här."]
             },
             {
+                image: "/src/images/havsniva/VattenVagor.png",
+                imageWidth: 3,
                 caption: "Nämen se här!",
                 paragraphs: ["Nedan ser du ett diagram på hur havsnivån varit sedan 1880-talet fram tills idag. Till vänster ser du stapeln för havsnivån och nere i liggande axeln ser du åren."]
             },
             {
+
                 caption: "Nämen se här!",
                 graphdata: ""
             },
             {
+
                 caption: "Såg du?",
                 paragraphs: ["I diagrammet kan man se hur mycket havsnivån stigit. Mellan 1980 och 2020 så har havsnivån stigit med t.ex. ????mm. Det är sedan dina föräldrar var född tills idag, galet va?"]
             },
