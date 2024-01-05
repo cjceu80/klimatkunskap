ba81a41578b0eae89b50323d2cfb0594ec0973e5
Yooniis <151764628+Yooniis@users.noreply.github.com>
2024-01-05 12:12:27 +0100

    j

--- src/components/QuizStart.jsx
+++ src/components/QuizStart.jsx
@@ -8,7 +8,7 @@ export default function QuizStart() {
     const cardStyle = {
         background: "rgba(255, 255, 255, 0.7)", // 0.7 represents 70% transparency
         borderRadius: "25px",
-
+        height: "400px",
         
       };
     return (
@@ -26,7 +26,7 @@ export default function QuizStart() {
             <Card className="p-5" style={cardStyle}>
                 <h2>Lätt som en plätt</h2>
                 <p>Perfekt för dig som precis börjat lära dig om klimatförändringar och miljöfrågor</p>
-
+                <br/>
                 <h2>Spelregler</h2>
                 <p>Antal frågor: 10 frågor</p>
                 <p>Du har obegränsat med tid</p>
@@ -36,7 +36,7 @@ export default function QuizStart() {
             <Card style={cardStyle} className="p-5">
                 <h2>Svårt o hårt</h2>
                 <p>För dig som känner dig säker på dina kunskaper och vill ha en utmaning</p>
-
+                <br/><br/>
                 <h2>Spelregler</h2>
                 <p>Antal frågor: 15 frågor</p>
                 <p>Du har 15 minuter på dig</p>
