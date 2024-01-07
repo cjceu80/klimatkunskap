e561d02711bf4c03c11ea003000332139e452b4b
ndaivan <ivan.ndayizeye.github2023@gmail.com>
2024-01-07 01:06:03 +0100

    ivan

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -26,8 +26,10 @@ const Feedback = () => {
                         value={feedback}
                         onChange={handleFeedbackChange}
                         placeholder="Skriv här..."
+                        rows={6} // Justera antalet rader efter behov
+                        cols={50} // Justera antalet kolumner efter behov
                     />
-                    <button onClick={handleSubmit} className="submit-button">Skicka</button>
+                    <button onClick={handleSubmit} style={{ borderRadius: "50px" ,  margin:"5px"}} className="submit-button">Skicka</button>
                     </div>
                 </div>
             </main>
